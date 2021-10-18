8c456bc3bd91fa043bfb8e5176e7666c2c613919
Vivek Pandey <keviv2698@gmail.com>
2021-10-18 20:58:12 +0530

    chore:all test done

--- String.py
+++ String.py
@@ -1,6 +1,19 @@
 def string_cal(str_num):
     if str_num =="":
         return "0"
+    elif len(str_num) > 1:
+        temp=0
+        for ch in str_num:
+            if ch =="-":
+                # print("negative", ch)
+                return "negatives not allowed -"+ str_num[str_num.index(ch)+1]
+        for ch in str_num:
+            
+            if(ch.isdigit()):
+                temp = temp + int(ch)
+        return temp
+        
     else:
-        return str_num
-# print(string_cal(5))
+        return int(str_num)
+
+print(string_cal("-8"))
--- test-string.py
+++ test-string.py
@@ -8,4 +8,12 @@ def test_empty_string():
 
 def test_number_string():
     result= string_cal("1")
-    assert result=="1"
+    assert result== 1
+
+def test_sum_of_numbers():
+    result= string_cal("1/2\n3;3")
+    assert result== 9
+
+def test_negative_number():
+    result=string_cal("-8")
+    assert result=="negatives not allowed -8"
